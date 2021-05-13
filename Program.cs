81e65ffd4eb855a901d2650b3b4364f49250e85d
Matthew Roberts <mxr2091@rit.edu>
2021-05-13 17:18:20 -0400

    added questions

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace example_questions_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // question 1
+            // Given two arrays of integers, find the common elements between them
+
+            // question 2
+            // Given an array of integers, find all pairs of numbers that add to a given value
+
+            // question 3
+            // Given a string, invert the positions of only the vowels
+
         }
     }
 }
